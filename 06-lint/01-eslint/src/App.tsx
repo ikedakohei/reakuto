d76130cdc2458b8b26e8932bc9507e32afc114f1
kohei <kouhei.ikeda@veriserve.co.jp>
2022-07-01 13:28:35 +0900

    6章 修正

--- 06-lint/01-eslint/src/App.tsx
+++ 06-lint/01-eslint/src/App.tsx
@@ -7,9 +7,7 @@ const App: FC = () => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
+        Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
         className="App-link"
